03a7033af5acf44d7b6e717db2c0ad0e8a496d16
mrcha033 <mrcha033@yonsei.ac.kr>
2025-07-25 17:42:27 +0900

    fix next.config.ts and tsconfig.json

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next";
-const withPWA = require("next-pwa");
+import withPWA from "next-pwa";
 import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
